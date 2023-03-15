f3886f30a78f3665d6dac4c6fbe4eaa1c6cd5ca7
Jocelyn <jocelynvargas123@yahoo.com>
2023-03-15 12:40:01 -0700

    A Lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31
+const num2 = 2
+    
+let multiply = num1 * num2
 
-// Write your code here
+
+const random = (Math.ceil(Math.random()*10)) 
+
+const num3 = 16
+const num4 = 4
+    
+let mod = num3 / num4
+
+let max = Math.max(1,10,20)
